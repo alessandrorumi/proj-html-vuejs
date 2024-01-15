0807853fe59ec7205381071a85cb4fdcaef348d7
TobiaRigon <rigontobia@gmail.com>
2024-01-15 18:28:58 +0100

    Fix: Import general.scss in style.main + Header structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,13 +7,63 @@ export default {
 <template>
   <header>
     <h1>Header</h1>
-    <ul>
+    <nav>
+      <ul>
       <li>
-        <router-link :to="{ name: 'contact' }">Vai a Contact</router-link>
+        <router-link :to="{ name: 'home' }">Home</router-link>
+      </li>
+      <li>
+        <a href="*">Blog</a>
+      </li>
+      <li>
+        <a href="*">Events</a>
+      </li>
+      <li>
+        <a href="*">Gallery</a>
+      </li>
+      <li>
+        <router-link :to="{ name: 'about' }">About</router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'contact' }">Contact</router-link>
+      </li>
+      <li>
+        <a href="*">Shop</a>
+      </li>
+      <li>
+        <i class="fa-solid fa-magnifying-glass"></i>
       </li>
     </ul>
+    </nav>
+    
   </header>
 </template>
 
 <style lang="scss" scoped>
+
+header{
+  background-color: black;
+  color: white;
+  nav{
+    ul{
+      display: flex;
+      justify-content: end;
+      list-style-type: none;
+      text-decoration: none;
+      
+      
+      li{
+        margin-left: 10px;
+        text-transform: uppercase;
+        a{
+        text-decoration: none;
+        text-transform: uppercase;
+        color: white;
+      }
+      }
+      
+    }
+    
+  }
+}
 </style>
