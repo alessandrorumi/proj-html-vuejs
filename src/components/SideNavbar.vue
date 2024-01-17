70fa609200ab600bb566514af3a2ccda1d70f75f
TobiaRigon <rigontobia@gmail.com>
2024-01-17 22:33:02 +0100

    Refactoring:SideNavbar

--- src/components/SideNavbar.vue
+++ src/components/SideNavbar.vue
@@ -27,7 +27,14 @@ export default {
         this.openSubMenus.push(subIndex); // Open the sub_submenu if it's closed
       }
     },
+
+    arrowClasses(index) {
+    return {
+      'fa-solid fa-chevron-down': !this.openSubMenus.includes(index),
+      'fa-solid fa-chevron-up': this.openSubMenus.includes(index),
+    }
   },
+},
 };
 </script>
 
@@ -40,13 +47,14 @@ export default {
           <router-link v-if="menuItem.link" :to="menuItem.link">{{ menuItem.name }}</router-link>
           <a v-else href="#" @click="toggleSubMenu(index)">
             {{ menuItem.name }}
-            <i v-if="menuItem.subItems" :class="{'fa-solid fa-chevron-down': !openSubMenus.includes(index), 'fa-solid fa-chevron-up': openSubMenus.includes(index)}"></i>
+            <i v-if="menuItem.subItems" :class="arrowClasses(index)"></i>
+
           </a>
           <ul v-if="menuItem.subItems && openSubMenus.includes(index)" class="submenu">
             <li v-for="(subItem, subIndex) in menuItem.subItems" :key="subIndex">
               <a href="#" @click="toggleSubSubMenu(subIndex)">
                 {{ subItem.name || subItem }}
-                <i v-if="subItem.subItems" :class="{'fa-solid fa-chevron-down': !openSubMenus.includes(subIndex), 'fa-solid fa-chevron-up': openSubMenus.includes(subIndex)}"></i>
+                <i v-if="subItem.subItems" :class="arrowClasses(subIndex)"></i>
               </a>
               <ul v-if="subItem.subItems && openSubMenus.includes(subIndex)" class="sub_submenu">
                 <li v-for="(subSubMenu, subSubMenuIndex) in subItem.subItems" :key="subSubMenuIndex">
@@ -64,75 +72,82 @@ export default {
 <style scoped lang="scss">
 @import '../styles/partials/_partials.scss';
 
-
-
-
-div {
+.side-navbar {
   overflow-y: auto;
   position: fixed;
   top: 0;
   right: -85%; 
   height: 100vh;
   width: 85%;
-  background-color:#090c10;
+  background-color: #090c10;
   text-align: end;
-  transition:all 0.3s ease ;
+  transition: all 0.3s ease;
   filter: opacity(0);
-  .open{
+
+  &.open {
     right: 0;
     filter: opacity(100%);
   }
 
-    .fa-x{
-        padding-top: 40px;
-        padding-right: 40px;
-    }
+  .fa-x {
+    padding-top: 40px;
+    padding-right: 40px;
+  }
 
-    ul{
-      
-        text-align: start;
-        list-style-type: none;
-        text-decoration: none;
-        border-bottom:1px solid rgb(71, 71, 71) ; 
-        li{
-          line-height: 50px;
-            position: relative;
-            border-bottom:1px solid rgb(71, 71, 71) ; 
-            a{
-              padding-left: 20px;
-              text-transform: capitalize;
-            text-decoration: none;
-            color: white;
-            display: flex;
-            justify-content: space-between;
-            .fa-chevron-up ,.fa-chevron-down{
-                border-left:1px solid rgb(71, 71, 71) ; 
-                line-height: 50px;
-                padding: 0 15px;
+  nav {
+    ul {
+      text-align: start;
+      list-style-type: none;
+      text-decoration: none;
+      border-bottom: 1px solid rgb(71, 71, 71);
 
-            }
+      li {
+        line-height: 50px;
+        position: relative;
+        border-bottom: 1px solid rgb(71, 71, 71);
 
+        a {
+          padding-left: 20px;
+          text-transform: capitalize;
+          text-decoration: none;
+          color: white;
+          display: flex;
+          justify-content: space-between;
 
-          
+          .fa-chevron-up, .fa-chevron-down {
+            border-left: 1px solid rgb(71, 71, 71);
+            line-height: 50px;
+            padding: 0 15px;
+          }
+
+          &:hover {
+            color: $main_orange;
+          }
         }
-        a:hover{
-             color: $main_orange;
+
+        ul {
+          border-top: 1px solid rgb(71, 71, 71);
+
+          li {
+            a:hover {
+              color: $main_orange;
             }
 
-
-            ul{
-              border-top: 1px solid rgb(71, 71, 71) ;
-              
+            ul {
+              li {
+                a:hover {
+                  color: $main_orange;
+                }
+              }
             }
+          }
+        }
+      }
+
+      li:last-of-type {
+        border-bottom: none; 
+      }
     }
-    li:last-of-type{
-      border-bottom:none ; 
-    }
+  }
 }
- 
-}
-
-
-
-
 </style>
