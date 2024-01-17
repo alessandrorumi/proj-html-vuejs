8ead11945eb355a91afcd900abbefeea15afc4ec
TobiaRigon <rigontobia@gmail.com>
2024-01-17 22:17:41 +0100

    Fix: .fa-chevron-up ,.fa-chevron-down in SideNavbar

--- src/components/SideNavbar.vue
+++ src/components/SideNavbar.vue
@@ -5,21 +5,27 @@ export default {
   props: ['menuItems'],
   data() {
     return {
-      showSubMenu: null,
-      showSubSubMenu: null,
+      openSubMenus: [],
     };
   },
   methods: {
     closeSideNav() {
       this.$emit('close');
+      this.openSubMenus = []; // Reset the array when closing the sidebar
     },
     toggleSubMenu(index) {
-      this.showSubMenu = this.showSubMenu === index ? null : index;
-      // Reset showSubSubMenu when toggling main submenu
-      this.showSubSubMenu = null;
+      if (this.openSubMenus.includes(index)) {
+        this.openSubMenus = this.openSubMenus.filter(i => i !== index); // Close the submenu if it's open
+      } else {
+        this.openSubMenus.push(index); // Open the submenu if it's closed
+      }
     },
     toggleSubSubMenu(subIndex) {
-      this.showSubSubMenu = this.showSubSubMenu === subIndex ? null : subIndex;
+      if (this.openSubMenus.includes(subIndex)) {
+        this.openSubMenus = this.openSubMenus.filter(i => i !== subIndex); // Close the sub_submenu if it's open
+      } else {
+        this.openSubMenus.push(subIndex); // Open the sub_submenu if it's closed
+      }
     },
   },
 };
@@ -32,11 +38,17 @@ export default {
       <ul>
         <li v-for="(menuItem, index) in menuItems" :key="index" class="menu_li">
           <router-link v-if="menuItem.link" :to="menuItem.link">{{ menuItem.name }}</router-link>
-          <a v-else href="#" @click="toggleSubMenu(index)">{{ menuItem.name }} <i v-if="menuItem.subItems" class="fa-solid fa-chevron-down"></i></a>
-          <ul v-if="menuItem.subItems && showSubMenu === index" class="submenu">
+          <a v-else href="#" @click="toggleSubMenu(index)">
+            {{ menuItem.name }}
+            <i v-if="menuItem.subItems" :class="{'fa-solid fa-chevron-down': !openSubMenus.includes(index), 'fa-solid fa-chevron-up': openSubMenus.includes(index)}"></i>
+          </a>
+          <ul v-if="menuItem.subItems && openSubMenus.includes(index)" class="submenu">
             <li v-for="(subItem, subIndex) in menuItem.subItems" :key="subIndex">
-              <a href="#" @click="toggleSubSubMenu(subIndex)">{{ subItem.name || subItem }} <i v-if="subItem.subItems" class="fa-solid fa-chevron-down"></i></a>
-              <ul v-if="subItem.subItems && showSubSubMenu === subIndex" class="sub_submenu" >
+              <a href="#" @click="toggleSubSubMenu(subIndex)">
+                {{ subItem.name || subItem }}
+                <i v-if="subItem.subItems" :class="{'fa-solid fa-chevron-down': !openSubMenus.includes(subIndex), 'fa-solid fa-chevron-up': openSubMenus.includes(subIndex)}"></i>
+              </a>
+              <ul v-if="subItem.subItems && openSubMenus.includes(subIndex)" class="sub_submenu">
                 <li v-for="(subSubMenu, subSubMenuIndex) in subItem.subItems" :key="subSubMenuIndex">
                   <a href="#">{{ subSubMenu.name || subSubMenu }}</a>
                 </li>
@@ -93,7 +105,7 @@ div {
             color: white;
             display: flex;
             justify-content: space-between;
-            .fa-chevron-down{
+            .fa-chevron-up ,.fa-chevron-down{
                 border-left:1px solid rgb(71, 71, 71) ; 
                 line-height: 50px;
                 padding: 0 15px;
