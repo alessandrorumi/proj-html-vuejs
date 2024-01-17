1aff26200917b7a7605fe148887032a64877d702
alessandrorumi <rumi.alessandro21@gmail.com>
2024-01-17 11:42:17 +0100

    Add: Scroll top new page

--- src/router.js
+++ src/router.js
@@ -1,26 +1,40 @@
-import {createRouter, createWebHistory} from 'vue-router';
-
+import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import About from './pages/About.vue';
 import Contact from './pages/Contact.vue';
+
 const router = createRouter({
-history: createWebHistory(),
-routes: [
-  {
-path: '/',
-name: 'home',
-component: AppHome
-},
-{
-path: '/about',
-name: 'about',
-component: About
-},
-{
-path: '/contact',
-name: 'contact',
-component: Contact
-},
-]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: AppHome,
+      beforeEnter(to, from, next) {
+        // Scrolla verso l'inizio della pagina prima di entrare nel componente
+        window.scrollTo(0, 0);
+        next();
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+      beforeEnter(to, from, next) {
+        window.scrollTo(0, 0);
+        next();
+      }
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+      beforeEnter(to, from, next) {
+        window.scrollTo(0, 0);
+        next();
+      }
+    }
+  ]
 });
+
 export { router };
